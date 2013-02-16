9ae91c368547774dec481c270c932d4e270b002e
Justin Mahar <justin.m.mahar@gmail.com>
2013-02-15 19:31:36 -0500

    In midst of design

---
+++ mahar/cap/TokenInfo.py
@@ -0,0 +1,12 @@
+
+class TokenAnalyzer:
+
+	def __init__(self, project_scanner):
+		self.project_scanner = project_scanner
+
+	def analyze_token_string(token_string):
+
+		pass
+		# Return a TokenInfo
+
+		
