7e4d977a88dc5cfa895357f1254b9cd811b31077
Justin Mahar <justin.m.mahar@gmail.com>
2013-02-17 22:41:53 -0500

    Fleshing out scanners a bit

--- CoffeePackageScanner.py
+++ CoffeePackageScanner.py
@@ -6,19 +6,18 @@ class CoffeePackageScanner:
 	def __init__(self, view_id_to_file_contents_dict, current_view_id, package_directories, excluded_directories, restricted_to_directories, built_in_types):
 		pass
 
-	def extract_expression_type(expression_string):
-		# Check for built-in expressions, like [a,b,c], "blah", {}, etc
-		# Start from left, use package scanner to determine types of each
-		# Return last expression type determined, or None if the chain can't be completed.
-		expression_type = ExpressionInfo("String", False)
-		return expression_type
+		def get_this_type(self):
+			return "String"
 
-	def get_definition_location(expression_string):
-		pass
+		def get_return_type_for_method(self):
+			return "String"
 
-	def tokenize_expression(expression_string):
-		# Collapse built-in expressions ("blah" to "", [a, b, c] to [], regex, etc)
-		# Replace starting @ with "this."
-		# Collapse params
-		# Split by dot operator
-		return ["this"]
+		def get_assignment_expression(self, token):
+			# Searching for "token = some_expression"
+			return "new String()"
+
+		def get_super_type_of(self):
+			return "Object"
+
+		def get_(self):
+			pass
